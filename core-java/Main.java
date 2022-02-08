eb1a1a0ccaf8facfc284e744909fe65d6d3f1931
Moulvi Kaif Ahmed <mka1904@outlook.com>
2022-02-08 15:14:29 +0530

    commit

--- core-java/Main.java
+++ core-java/Main.java
@@ -1,7 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("git");
+        byte b = 11;
+        short s = 33;
+        int i = 45;
+        long l = 566;
+        float f = 45.4f;
+        double d = 69.69;
+        boolean bl = true;
+        char c = 'a';
+        System.out.println(b + " " + s + " " + i + " " + l + " " + f + " " + d + " " + bl + " " + c + " ");
     }
 
 }
